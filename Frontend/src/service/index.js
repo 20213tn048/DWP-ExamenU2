a30def414e45f09c8ff4be317dc0457092b15311
Yexzari <20203tn141@utez.edu.mx>
2024-02-29 16:41:58 -0600

    nuevo

--- Frontend/src/service/index.js
+++ Frontend/src/service/index.js
@@ -3,7 +3,11 @@ import axios from 'axios';
 let URL_BASE ="http://localhost:8080/"
 
 const getBooks = async()=>{
-
+    try {
+       const res= await axios.get(`${}`)
+    } catch (error) {
+        
+    }
 }
 const getBooksByAuthor = async()=>{
         
