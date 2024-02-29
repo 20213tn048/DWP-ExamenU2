b452e401aa0a9ef5a89d9f3cc08fa41b6a111806
Yexzari <20203tn141@utez.edu.mx>
2024-02-29 16:47:18 -0600

    nue

--- Frontend/src/service/index.js
+++ Frontend/src/service/index.js
@@ -4,20 +4,59 @@ let URL_BASE ="http://localhost:8080/"
 
 const getBooks = async()=>{
     try {
-       const res= await axios.get(`${}`)
+       const res= await axios.get(`${URL_BASE}api/books/`);
+       return res.data;
     } catch (error) {
-        
+        throw error;
     }
 }
 const getBooksByAuthor = async()=>{
-        
+            try{
+        const res = await axios.get(`${URL_BASE}api/books/author`)
+        return res.data;
+    }catch(error){
+        throw error;
+    }
 }
 const getBooksByDate = async()=>{
-        
+            try{
+        const res = await axios.get(`${URL_BASE}api/books/publicationDate`)
+        return res.data;
+    }catch(error){
+        throw error;
+    }
 }
 const addNewBook = async()=>{
-        
+    try{
+        const res = await axios.get(`${URL_BASE}api/books/`,book)
+        return res.data;
+    }catch(error){
+        throw error;
+    }
 }
-const updateBook = async()=>{
-        
+const updateBook = async(id, book) =>{
+    try{
+        const res = await axios.put(`${URL_BASE}api/books/${id}`,book);
+        return res.data
+    }catch(error){
+        throw error;
+    }
+}
+
+const deleteBook = async(id) =>{
+    try{
+        const res = await axios.delete(`${URL_BASE}api/books/${id}`);
+        return res.data;
+    }catch(error){
+        throw error;
+    }
+}
+
+export {
+    getBooks,
+    getBooksByAuthor,
+    getBooksByDate,
+    addNewBook,
+    updateBook,
+    deleteBook
 }
