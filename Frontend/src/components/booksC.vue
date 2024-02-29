5f74e32b0656730bb075376899581a2c429db694
CrisSoven <20213tn044@utez.edu.mx>
2024-02-29 16:31:56 -0600

    Front

---
+++ Frontend/src/components/booksC.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-button @click="sortByAuthor">Ordenar por autor</b-button>
+    <b-button @click="sortByDate">Ordenar por fecha</b-button>
+    <b-button @click="insert">Agregar libro</b-button>
+    <b-button @dragover.prevent @drop="edit">Editar</b-button>
+    <b-button @dragover.prevent @drop="deleteBook">Eliminar</b-button>
+
+  </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        sortByAuthor(){
+            this.$emit('sortByAuthor');
+        },
+        sortByDate(){
+            this.$emit('sortByDate');
+        },
+        insert(){
+            this.$emit('insert');
+        },
+        edit(){
+            this.$emit('edit');
+        },
+        deleteBook(){
+            this.$emit('deleteBook');
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Frontend/src/service/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+let URL_BASE ="http://localhost:8080/"
+
+const getBooks = async()=>{
+
+}
+const getBooksByAuthor = async()=>{
+        
+}
+const getBooksByDate = async()=>{
+        
+}
+const addNewBook = async()=>{
+        
+}
+const updateBook = async()=>{
+        
+}
